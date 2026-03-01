18a19febf1ef8cfe44ebb026b7bcfa2a7b3dad94
rwirba <rwirba@protonmail.com>
2026-03-01 15:04:29 +0300

    test

--- app/scan_loop.sh
+++ app/scan_loop.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-while true; do
-    /app/fetch_and_scan.sh
-    python3 /app/generate_report.py
-    sleep 300   # 5 minutes
-done
+echo "Background loop disabled for initial setup. Run manual scans."
+# while true; do
+#     /app/fetch_and_scan.sh
+#     python3 /app/generate_report.py
+#     sleep 300
+# done
